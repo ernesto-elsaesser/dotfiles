8bba21a031ef3846aa29ff378006e2c13c313355
Ernesto Elsäßer <ernesto.elsaesser@immatics.com>
2021-03-24 14:08:23 +0100

    Improve and comment git aliases

--- git-env/aliases.sh
+++ git-env/aliases.sh
@@ -1,14 +1,13 @@
-function conf {
-  git config --global user.email "ernesto.elsaesser@me.com"
-  git config --global user.name "Ernesto Elsaesser"
-}
-alias id='conf'
-
 alias f='git fetch --all'
 
 alias pl='git pull --ff-only'
 
 alias s='git status -s'
+
+alias co='git checkout'
+
+# create new branch
+alias cb='git checkout -b'
 
 alias a='git add --all; s'
 
@@ -18,15 +17,13 @@ function commit_as {
 }
 alias c='commit_as'
 
-alias ca='git commit --amend --no-edit'
-
-alias d='git diff'
-alias ds='git diff --staged'
-
 alias p='git push'
-alias pu='git push -u origin HEAD'
 alias pf='git push --force'
 
+# push new branch to upstream repo
+alias pu='git push -u origin HEAD'
+
+# push all changes (!)
 function push_all {
     git add --all
     git commit -m "$*"
@@ -34,21 +31,46 @@ function push_all {
 }
 alias P='push_all'
 
-alias m='git merge --no-edit'
-alias mm='git merge --no-edit origin/master'
+# delete branch
+alias bd='git branch -d'
 
-alias co='git checkout'
-alias cb='git checkout -b'
+alias d='git diff'
+alias ds='git diff --staged'
 
+# show hash/message/author/time of last 10 commits 
 alias l='git log -10 --pretty=format:"%h %s (%an %ar)"'
 
+# add changes to last commit
+alias ca='git commit --amend --no-edit'
+
+# merge upsteam branch into current
+alias m='git merge --no-edit'
+
+# merge master branch into current
+alias mm='git merge --no-edit origin/master'
+
+# show branches - a = remote ones, v = incl. last  commit
 alias b='git branch'
 alias ba='git branch -a'
 alias bv='git branch -vv'
-alias bd='git branch -d'
 
 alias rb='git rebase'
+
+# rebase the last n commits interactively ('ri2')
 alias ri='git rebase -i HEAD~'
 
+# undo the last n commits ('u3')
 alias u='git reset HEAD~'
+
+# discard all uncommit changes
 alias rh='git reset --hard'
+
+# print commit hash of the current commit
+alias h='git rev-parse HEAD'
+
+# configure git user
+function conf {
+  git config --global user.email "ernesto.elsaesser@me.com"
+  git config --global user.name "Ernesto Elsaesser"
+}
+alias id='conf'
