fcaa5cbe55c4558b676df7dcd3e4403b553c57d4
Ernesto Elsäßer <ernesto.elsaesser@immatics.com>
2022-03-24 13:22:08 +0100

    P Install prebuilt labextension

--- jupyterlab/install.sh
+++ jupyterlab/install.sh
@@ -1,2 +1,3 @@
+EXTDIR=~/anaconda3/share/jupyter/labextensions/vimrc/
 cp shortcuts.json ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/shortcuts.jupyterlab-settings
-[ "$1" = "ext" ] && jupyter labextension build
+[ "$1" = "ext" ] && mkdir $EXTDIR && cp -r prebuilt/* $EXTDIR
