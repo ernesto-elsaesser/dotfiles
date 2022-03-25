560cf669a2bce644e0abc5024e58b88189d8c9fb
Ernesto Elsäßer <ernesto.elsaesser@immatics.com>
2022-03-25 15:22:13 +0100

    Fix labext installation

--- jupyterlab/install.sh
+++ jupyterlab/install.sh
@@ -1,3 +1,3 @@
 EXTDIR=~/anaconda3/share/jupyter/labextensions/vimrc/
 cp shortcuts.json ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/shortcuts.jupyterlab-settings
-[ "$1" = "ext" ] && mkdir $EXTDIR && cp -r prebuilt/* $EXTDIR
+[ "$1" = "ext" ] && mkdir -p $EXTDIR && cp -r prebuilt/* $EXTDIR
