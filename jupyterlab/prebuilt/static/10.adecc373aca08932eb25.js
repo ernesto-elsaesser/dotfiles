211347d0567473c485198f7ca8847fa3582fe03d
Ernesto Elsäßer <ernesto.elsaesser@immatics.com>
2022-03-25 16:25:41 +0100

    Fix vim labextension

--- jupyterlab/prebuilt/static/10.adecc373aca08932eb25.js
+++ jupyterlab/prebuilt/static/10.adecc373aca08932eb25.js
@@ -1,1 +1,14 @@
-"use strict";(self.webpackChunkvimrc=self.webpackChunkvimrc||[]).push([[10],{10:(r,i,e)=>{e.r(i),e.d(i,{default:()=>a});const a={id:"vimrc",autoStart:!0,requires:[e(768).ICodeMirror],activate:function(r,i){i.ensureVimKeymap().then((function(){i.CodeMirror.Vim.map("<Space>",":w","normal"),i.CodeMirror.Vim.map("ö","<Esc>","insert"),i.CodeMirror.Vim.map("ö","<Esc>","visual"),console.log("vim mappings installed!")}))}}}}]);
+"use strict";(self.webpackChunkvimrc=self.webpackChunkvimrc||[]).push([[10],{10:(r,i,e)=>{e.r(i),e.d(i,{default:()=>a});const a = {
+    id: "vimrc",
+    autoStart: true,
+    requires: [e(768).ICodeMirror],
+    activate: function(r,i) {
+        i.ensureVimKeymap().then((function() {
+            const api = i.CodeMirror.Vim;
+            api.map("<Space>",":w","normal");
+            const wrapped = api.findKey;
+            api.findKey = function(cm, key, origin) { if (key == "ö") key = '<Esc>'; return wrapped(cm, key, origin); };
+            console.log('vim patched');
+        }))
+    }
+}}}]);
