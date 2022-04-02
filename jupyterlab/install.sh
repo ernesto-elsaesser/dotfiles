326e5e9cba98b1d5f91f2f847e64162389089699
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2022-04-02 20:48:10 +0200

    Improve jlab install script

--- jupyterlab/install.sh
+++ jupyterlab/install.sh
@@ -1,3 +1,3 @@
-EXTDIR=~/anaconda3/share/jupyter/labextensions/vimrc/
+EXTDIR="$CONDA_PREFIX/share/jupyter/labextensions/vimrc/"
 cp shortcuts.json ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/shortcuts.jupyterlab-settings
 [ "$1" = "ext" ] && mkdir -p $EXTDIR && cp -r prebuilt/* $EXTDIR
