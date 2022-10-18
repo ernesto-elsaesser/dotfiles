f75825318b423615bfc83504ca2d8e248a92c9ca
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2022-10-18 22:27:41 +0200

    Disable jupyter extension

--- jupyterlab/install.sh
+++ jupyterlab/install.sh
@@ -1,4 +1,6 @@
 #  jupyter lab --ContentsManager.allow_hidden=True --FileCheckpoints.checkpoint_dir=~/.jupyter/check
-EXTDIR="$CONDA_PREFIX/share/jupyter/labextensions/vimrc/"
+
 cp shortcuts.json ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/shortcuts.jupyterlab-settings
-mkdir -p $EXTDIR && cp -r prebuilt/* $EXTDIR
+
+#EXTDIR="$CONDA_PREFIX/share/jupyter/labextensions/vimrc/"
+#mkdir -p $EXTDIR && cp -r extension/* $EXTDIR
