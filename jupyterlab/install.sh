67ae12318793426be10299ae6c05397f8ccc0a66
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2022-12-17 17:47:24 +0000

    Fix jupyter installation script

--- jupyterlab/install.sh
+++ jupyterlab/install.sh
@@ -1,7 +1,7 @@
 #  jupyter lab --ContentsManager.allow_hidden=True --FileCheckpoints.checkpoint_dir=~/.jupyter/check
 
 SETTINGS_DIR="$HOME/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension"
-[ -d $SETTINGS_DIR ] || mkdir $SETTINGS_DIR
+[ -d $SETTINGS_DIR ] || mkdir -p $SETTINGS_DIR
 cp shortcuts.json "$SETTINGS_DIR/shortcuts.jupyterlab-settings"
 
 #EXT_DIR="$CONDA_PREFIX/share/jupyter/labextensions/vimrc/"
