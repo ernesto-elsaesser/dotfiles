3020a8fe870effb2159f4f005bfaa9e3de82c45a
Ernesto Elsäßer <ernesto.elsaesser@immatics.com>
2022-12-19 10:35:38 +0100

    Update jupyter server config, cleanup

--- jupyterlab/install.sh
+++
@@ -1,8 +0,0 @@
-#  jupyter lab --ContentsManager.allow_hidden=True --FileCheckpoints.checkpoint_dir=~/.jupyter/check
-
-SETTINGS_DIR="$HOME/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension"
-[ -d $SETTINGS_DIR ] || mkdir -p $SETTINGS_DIR
-cp shortcuts.json "$SETTINGS_DIR/shortcuts.jupyterlab-settings"
-
-#EXT_DIR="$CONDA_PREFIX/share/jupyter/labextensions/vimrc/"
-#mkdir -p $EXT_DIR && cp -r extension/* $EXT_DIR
--- key-mapping/script.ahk
+++ key-mapping/script.ahk
@@ -2,13 +2,3 @@
 SendMode Input
 
 Capslock::Control
-
-^Space::Send, {Backspace}
-^m::Send, {Enter}
-
-!d::Send, {Right}
-!a::Send, {Left}
-!w::Send, {Up}
-!s::Send, {Down}
-!q::Send, {Home}
-!e::Send, {End}
