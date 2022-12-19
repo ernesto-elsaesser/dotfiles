c7e22a3a1e81970a6868f6df1d067296fdbe3d77
Ernesto Elsäßer <ernesto.elsaesser@immatics.com>
2022-12-19 10:45:41 +0100

    Use python jupyter server config

--- jupyterlab/jupyter_server_config.py
+++ jupyterlab/jupyter_server_config.py
@@ -1,19 +1,22 @@
-{
-  "ServerApp": {
-    "password": "...",
-    "password_required": true,
-    "ip": "*",
-    "notebook-dir": "/home/ee",
-    "open_browser": false
-  },
-  "NotebookApp": {
-    "certfile": "/etc/ssl/certs/server.crt",
-	"keyfile": "/etc/ssl/private/server.key"
-  },
-  "ContentsManager": {
-    "allow_hidden": true
-  },
-  "FileCheckpoints": {
-    "checkpoint_dir": "/home/ee/.jupyter/check"
-  }
+import os
+import ssl
+
+home_dir = os.environ["HOME"]
+
+c.ServerApp.password = "..."
+c.ServerApp.password_required = True
+c.ServerApp.ip = "*"
+c.ServerApp.notebook_dir = home_dir
+c.ServerApp.open_browser = False
+
+c.NotebookApp.certfile = "/etc/ssl/certs/server.crt"
+c.NotebookApp.keyfile = "/etc/ssl/private/server.key"
+c.NotebookApp.ssl_options = {
+    "ssl_version": ssl.PROTOCOL_TLSv1_2
 }
+
+c.ContentsManager.allow_hidden = True
+
+checkpoint_dir = os.path.join(home_dir, ".jupyter", "check")
+os.makedirs(checkpoint_dir, exist_ok=True)
+c.FileCheckpoints.checkpoint_dir = checkpoint_dir
