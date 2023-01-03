1c066852a96df3b7ec130ea7ffb6d76f54673b08
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2023-01-03 18:38:10 +0000

    Update jupyter server config

--- jupyterlab/jupyter_server_config.py
+++ jupyterlab/jupyter_server_config.py
@@ -2,12 +2,11 @@ import os
 import ssl
 
 home_dir = os.environ["HOME"]
+checkpoint_dir = os.path.join(home_dir, ".jupyter", "check")
+os.makedirs(checkpoint_dir, exist_ok=True)
 
-# set password in json config file: ServerApp.password
-c.ServerApp.password_required = True
 c.ServerApp.ip = "*"
 c.ServerApp.root_dir = home_dir
-#c.ServerApp.notebook_dir = home_dir - deprecated
 c.ServerApp.certfile = "/etc/ssl/certs/server.crt"
 c.ServerApp.keyfile = "/etc/ssl/private/server.key"
 c.ServerApp.ssl_options = {
@@ -15,10 +14,11 @@ c.ServerApp.ssl_options = {
 }
 c.ServerApp.open_browser = False
 
+c.PasswordIdentityProvider.password_required = True
+c.PasswordIdentityProvider.hashed_password = "..."
+
 c.FileContentsManager.allow_hidden = True
 c.FileContentsManager.delete_to_trash = False
 c.FileContentsManager.always_delete_dir = True
 
-checkpoint_dir = os.path.join(home_dir, ".jupyter", "check")
-os.makedirs(checkpoint_dir, exist_ok=True)
 c.FileCheckpoints.checkpoint_dir = checkpoint_dir
