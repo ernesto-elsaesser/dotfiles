c04bf1aa31faa1e8798c5f30b5ea7ea5cb92249b
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2023-02-05 03:37:32 +0100

    Fix jupyter config, git config

--- jupyterlab/configure.sh
+++ jupyterlab/configure.sh
@@ -1,1 +1,3 @@
-cp -rf user-settings/* "$HOME/.jupyter/lab/user-settings/@jupyterlab"
+SETTINGS_DIR="$HOME/.jupyter/lab/user-settings/@jupyterlab"
+mkdir -p "$SETTINGS_DIR"
+cp -rf user-settings/* "$SETTINGS_DIR"
