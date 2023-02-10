17de4f1a5965ba89ad2954f749d323b1b0c4644b
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2023-02-10 12:15:27 +0100

    Rename alias script, extend install script

--- install.sh
+++ install.sh
@@ -2,4 +2,7 @@
 
 cd "${BASH_SOURCE%/*}"
 DOT_DIR=`pwd`
-echo -e "source $DOT_DIR/vimrc\n" > /etc/vim/vimrc
+echo -e ". $DOT_DIR/aliases.sh\n" >> ~/.bashrc
+echo -e ". $DOT_DIR/aliases.sh\n" >> ~/.profile
+echo -e ". $DOT_DIR/vimrc\n" > /etc/vim/vimrc
+cp gitconfig ~/.gitconfig
