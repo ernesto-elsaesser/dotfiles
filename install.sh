c776aad5d7d3508cb6d30a3d2c21a76d678da2b5
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2023-02-21 12:12:35 +0100

    Improve install script

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "${BASH_SOURCE%/*}"
-DOT_DIR=`pwd`
-echo -e ". $DOT_DIR/aliases.sh\n" >> ~/.bashrc
-echo -e ". $DOT_DIR/aliases.sh\n" >> ~/.profile
-echo -e ". $DOT_DIR/vimrc\n" > /etc/vim/vimrc
-cp gitconfig ~/.gitconfig
