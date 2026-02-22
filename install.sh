080252e1b4b30adaeec8107acf077a3e105a8760
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2026-02-22 20:39:39 +0100

    Remove zsh support

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-rcfile=".bashrc"
-[[ "$(uname)" == "Darwin" ]] && rcfile=".zshrc"
+echo "Loading aliases in .bashrc ..."
+echo "source $PWD/aliases.sh" >> "$HOME/.bashrc"
 
-echo "Loading aliases in $rcfile ..."
-echo "source $PWD/aliases.sh" >> "$HOME/$rcfile"
-
-echo "Loading prompt in $rcfile ..."
-echo "source $PWD/prompt.sh" >> "$HOME/$rcfile"
+echo "Loading prompt in .bashrc ..."
+echo "source $PWD/prompt.sh" >> "$HOME/.bashrc"
 
 echo "Generating .gitconfig ..."
 cat <<EOF > "$HOME/.gitconfig"
 [user]
-    name = Ernesto Elsäßer
     email = ernesto.elsaesser@gmail.com
 [include]
     path = $PWD/gitconfig
