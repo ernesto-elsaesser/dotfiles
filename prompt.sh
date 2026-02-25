3992c5bbafe50d11643d4eb551b5bd13acd86b4a
Ernesto Elsäßer <ernesto.elsaesser@immatics.com>
2026-02-25 10:18:44 +0100

    Include conda env in prompt

--- prompt.sh
+++ prompt.sh
@@ -1,8 +1,10 @@
 C1='\[\e[32m\]'
 C2='\[\e[94m\]'
 CX='\[\e[0m\]'
-[[ "$(type -t __git_ps1)" = 'function' ]] && PROMPT_COMMAND='__git_ps1 "${C1}\u@\h${CX}:${C2}\w${CX}" "\\\$ "'
-GIT_PS1_SHOWDIRTYSTATE=1
-GIT_PS1_SHOWSTASHSTATE=1
-GIT_PS1_SHOWUNTRACKEDFILES=1
-GIT_PS1_SHOWUPSTREAM="auto"
+if [[ "$(type -t __git_ps1)" = 'function' ]]; then
+    PROMPT_COMMAND='__git_ps1 "${CONDA_PREFIX:+($CONDA_DEFAULT_ENV) }${C1}\u@\h${CX}:${C2}\w${CX}" "\\\$ "'
+    GIT_PS1_SHOWDIRTYSTATE=1
+    GIT_PS1_SHOWSTASHSTATE=1
+    GIT_PS1_SHOWUNTRACKEDFILES=1
+    GIT_PS1_SHOWUPSTREAM="auto"
+fi
