ad0e629c66de63cf4dff160697828338bc892e92
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2026-03-16 12:06:22 +0100

    Remove git from prompt

--- prompt.sh
+++ prompt.sh
@@ -1,10 +1,3 @@
-C1='\[\e[32m\]'
-C2='\[\e[32m\]'
-CX='\[\e[0m\]'
-if [[ "$(type -t __git_ps1)" = 'function' ]]; then
-    PROMPT_COMMAND='__git_ps1 "${CONDA_PREFIX:+($CONDA_DEFAULT_ENV) }${C1}\u@\h${CX}:${C2}\w${CX}" "\\\$ "'
-    GIT_PS1_SHOWDIRTYSTATE=1
-    GIT_PS1_SHOWSTASHSTATE=1
-    GIT_PS1_SHOWUNTRACKEDFILES=1
-    GIT_PS1_SHOWUPSTREAM="auto"
-fi
+COL='\[\e[32m\]'
+END='\[\e[0m\]'
+PS1="${CONDA_PREFIX:+($CONDA_DEFAULT_ENV) }${COL}\u@\h${END}:\w $ "
