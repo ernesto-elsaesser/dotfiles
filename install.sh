f4df94aaf0c1ee6d6c06222108da59270a0c08f4
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2026-03-27 16:09:27 +0100

    Adjust install script

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+DOT_DIR="$HOME/dotfiles"
+
 echo "Installing custom bashrc ..."
-echo "source $PWD/bashrc" >> "$HOME/.bashrc"
+echo "source $DOT_DIR/bashrc" >> "$HOME/.bashrc"
 
 echo "Generating .gitconfig ..."
 cat <<EOF > "$HOME/.gitconfig"
 [user]
     email = ernesto.elsaesser@gmail.com
 [include]
-    path = $PWD/gitconfig
+    path = $DOT_DIR/gitconfig
 EOF
 
 echo "Linking .tmux.conf ..."
 rm -f "$HOME/.tmux.conf"
-ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
+ln -s "$DOT_DIR/tmux.conf" "$HOME/.tmux.conf"
 
 echo "Linking .vimrc ..."
 rm -f "$HOME/.vimrc"
-ln -s "$PWD/vimrc" "$HOME/.vimrc"
+ln -s "$DOT_DIR/vimrc" "$HOME/.vimrc"
 
